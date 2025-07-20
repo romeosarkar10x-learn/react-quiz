006c75cd8cef0ee6d74e836518bcbf6b71d05aa2
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-07-20 12:49:24 +0530

    set up project

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+export default {
+    tabWidth: 4,
+    printWidth: 120,
+    semi: true,
+    quoteProps: "consistent",
+    trailingComma: "all",
+    bracketSameLine: true,
+    arrowParens: "avoid",
+    htmlWhitespaceSensitivity: "strict",
+    singleAttributePerLine: true,
+
+    overrides: [
+        {
+            files: ["**/*.yaml", "**/*.yml"],
+            options: {
+                tabWidth: 4,
+            },
+        },
+    ],
+};
