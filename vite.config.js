04898c96053e1430010f53d5cd2ed1687ac0b47f
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-07-22 01:33:11 +0530

    checking GITHUB_REPOSITORY env variable in github actions

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
+console.log("hello from vite config");
+console.log(process.env.GITHUB_REPOSITORY);
+
 export default defineConfig({
     plugins: [react()],
+
+    base: process.env.GITHUB_REPOSITORY,
 });
