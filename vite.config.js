5d0fe128e2417dec56fe7c7e513c59dbb7618ea7
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-07-22 01:57:19 +0530

    set base-url to /<repo-name>, so that i matches with the github pages url

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import child_process from "child_process";
 
-// https://vite.dev/config/
-const REPO_NAME = process.env.GITHUB_REPOSITORY.split("/").at(-1);
-console.log("REPO_NAME:", REPO_NAME);
+let repo = process.env?.GITHUB_REPOSITORY?.split("/")?.at(-1);
+
+if (!repo) {
+    const branch = child_process.execSync("git branch --show-current").toString().trim();
+    const remote = child_process.execSync(`git config --get branch.${branch}.remote`).toString().trim();
+    const url = child_process.execSync(`git remote get-url ${remote}`).toString().trim();
+    repo = url.split("/").at(-1).split(".").slice(0, -1).join(".");
+}
+
+console.log("Base URL:", repo);
 
 export default defineConfig({
     plugins: [react()],
-
-    base: process.env.GITHUB_REPOSITORY,
+    base: "/" + repo,
 });
